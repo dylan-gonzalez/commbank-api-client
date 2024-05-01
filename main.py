2906edd864e2c093257be138e1abc8e68bcd8516
dylan-gonzalez <dylcg10@gmail.com>
2024-05-02 09:04:53 +1000

    use secrets.yaml

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from commbank_api_client import create_client, Client
 import asyncio
+import yaml
 
+secrets = yaml.load(open('./secrets.yaml'), Loader=yaml.Loader)
 
 async def main():
     # initiate client without async context manager and log in
@@ -9,9 +11,11 @@ async def main():
     # await client.close()
 
     # Initiate client with async context manager and log in (recommended)
-    async with Client("username", "password") as client:
+    async with Client(secrets['username'], secrets['password']) as client:
         # get account ID
         account_id = (await client.get_accounts())[0].id
+
+        print(f"account_id: {account_id}")
 
         # get transactions (first page)
         transactions = await client.get_transactions(account_id)
